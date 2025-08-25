a2d908ac85c90796b0c8de212c77f25f6cb0f76f
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-08-25 11:38:34 +0900

    CI: add vercel settings

--- vite.config.js
+++ vite.config.js
@@ -15,10 +15,9 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     proxy: {
-      // '/api'로 시작하는 요청을 백엔드 서버로 전달합니다.
       '/api': {
-        target: 'http://localhost:8080', // 백엔드 서버 주소
-        changeOrigin: true, // CORS 오류를 방지하기 위해 필요합니다.
+        target: 'http://localhost:8080',
+        changeOrigin: true,
       },
     },
   },
